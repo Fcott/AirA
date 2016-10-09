0aaa1fafedbd8087d45059f2983411ba55b9f2e2
Fcott <f.f.f.kt13@gmail.com>
2016-10-09 17:16:24 -0500

    Added Review feature

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,14 @@ Rails.application.routes.draw do
 
   resources :rooms do
     resources :reservations, only: [:create]
+    resources :reviews, only: [:create, :destroy]
   end
 
   resources :conversations, only: [:index, :create] do
     resources :messages, only: [:index, :create]
   end
+
+
 
   get '/preload' => 'reservations#preload'
   get '/preview' => 'reservations#preview'
