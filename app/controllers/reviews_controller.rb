f77b5ddc655a5aba59e8d937b053044bfe5b4844
Fcott <f.f.f.kt13@gmail.com>
2016-10-10 08:36:11 -0500

    reset

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,14 +3,21 @@ class ReviewsController < ApplicationController
   def create
     @review = current_user.reviews.create(review_params)
     redirect_to @review.room
+    # respond_to do |format|
+    #   format.js
+    # end
   end
 
   def destroy
     @review = Review.find(params[:id])
     room = @review.room
     @review.destroy
-    
+
     redirect_to room
+
+    # respond_to do |format|
+    #   format.js
+    # end
   end
 
   private
